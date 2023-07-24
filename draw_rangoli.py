83b52c554da9ee60076ccf8a4cce954c7f1cd2e0
arun <arun.bme2@gmail.com>
2023-07-24 16:09:48 +0530

    rangoli

--- draw_rangoli.py
+++ draw_rangoli.py
@@ -1,9 +1,17 @@
 def print_rangoli(size):
     # your code goes here
     alphabets = list(reversed(list(map(chr, range(97, 123)))[:size]))
-    for i in range(1,2*size-1):
-        sliced_string = '-'.join(alphabets[0:i] + list(reversed(alphabets))[0:i-1])
+    index = 1
+    for i in range(1,2*size):
         if i < size:
+            sliced_string = '-'.join(alphabets[0:i] + list(reversed(alphabets[0:i-1])))
+            print(sliced_string.center((((size + (size - 1)) * 2) - 1) ,'-'))
+        elif i == size:
+            sliced_string = '-'.join(alphabets[0:i] + list(reversed(alphabets[0:i-1])))
+            print(sliced_string.center((((size + (size - 1)) * 2) - 1) ,'-'))
+        elif i > size:
+            index -= 1
+            sliced_string = '-'.join(alphabets[0:i][0:index-1] + list(reversed(alphabets[0:i-1][0:index-2])))
             print(sliced_string.center((((size + (size - 1)) * 2) - 1) ,'-'))
         
         
