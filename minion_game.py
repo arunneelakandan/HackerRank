4ff121ebcbc7c2020dfeb69d44504f69dffedfad
arun <arun.bme2@gmail.com>
2023-07-25 14:11:02 +0530

    minion game

--- minion_game.py
+++ minion_game.py
@@ -1,12 +1,29 @@
+from itertools import combinations
+
+
 def minion_game(string):
     stuart = 0
     kevin = 0
     
     vowels = ['a','e','i','o','u']
     
-    for i in range(len(s)):
-        if i in vowels:
+    for r in range(0, len(string)):
+        for l in range(1,len(string)+1):
+            startwith_vowel = False
+            if string[r:l] and string[r:l][0] in vowels:
+                startwith_vowel = True
+
+        
+            if startwith_vowel:
+                kevin += 1
+                
+            else:
+                stuart += 1
             
+    if stuart > kevin:
+        print('Stuart',stuart)
+    else:
+        print('Kevin',kevin)        
     
 
 if __name__ == '__main__':
